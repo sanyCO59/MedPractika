03fbfbeaa26d893374af2fa79029c6196282e6c1
sanyCO <sanyalaz59@gmail.com>
2023-03-24 10:15:21 +0300

    Версия 1.1

--- app/src/main/java/com/example/medpractika/MainActivity.java
+++ app/src/main/java/com/example/medpractika/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.medpractika;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        }, 0, 3000);
+    }
+    public void nextPage(){
+        Intent intent = new Intent(MainActivity.this, Onboard.class);
+        startActivity(intent);
     }
 }
