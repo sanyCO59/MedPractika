a486543bae19367c1731b4601ecb4485eaefa983
sanyCO <sanyalaz59@gmail.com>
2023-04-01 12:57:01 +0300

    Вторая сессия

---
+++ app/src/main/java/com/example/medpractika/CreatePass.java
@@ -0,0 +1,18 @@
+package com.example.medpractika;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreatePass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_pass);
+    }
+
+    public void skip(View view) {
+    }
+}
